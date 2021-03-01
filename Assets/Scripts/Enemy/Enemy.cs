a1b5eaa34173c4a4f3c204b4c4009b58c81ecbe2
Samuel Tayman <nicholastayman@live.com>
2021-03-01 16:21:31 -0500

    Initial Commit
    Base commit containing all files from Unity Teams

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    GameObject player;
+    public bool spinShot = false;
+    public bool followShot = false;
+    public int maxDistance = 50;
+
+    int curShot = 0;
+    int timerShot = 0;
+    
+    Monster monster;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        monster = GetComponent<Monster> ();
+    }
+
+    void Update()
+    {
+        if(Time.timeScale != 0)
+        {
+            attackPlayer();
+        }
+    }
+
+    void attackPlayer()
+    {
+        float distanceFromPlayer = Vector3.Distance(player.transform.position, transform.position);;
+
+        if( distanceFromPlayer < maxDistance)
+        {
+            if(timerShot == monster.shotDelay)
+            {
+                if(followShot)
+                {
+                    if(transform.GetChild(0).childCount > 1)
+                    {
+                        Transform ShotPattern = transform.GetChild(0);                    
+
+                        GameObject playerPosition = new GameObject(); 
+                        playerPosition.transform.position = transform.position;
+
+                        Vector3 difference = player.transform.position - playerPosition.transform.position;
+                        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+
+                        for (int i = 0; i < ShotPattern.childCount; i++)    {
+                            Transform shotPosition = ShotPattern.GetChild(i);
+                            shotPosition.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ- 90f);
+                            monster.Shot (shotPosition);
+                        }
+
+                        Destroy(playerPosition);        
+
+                    }
+                    else
+                    {
+                        GameObject playerPosition = new GameObject(); 
+                        playerPosition.transform.position = transform.position;
+
+                        Vector3 difference = player.transform.position - playerPosition.transform.position;
+                        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+                        playerPosition.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ- 90f) ;
+
+                        monster.Shot (playerPosition.transform);             
+
+                        Destroy(playerPosition);        
+                    }
+                }
+                else if(spinShot)
+                {
+                    Transform ShotPattern = transform.GetChild(0);
+                    Transform shotPosition = ShotPattern.GetChild(curShot);
+                    monster.Shot (shotPosition);
+                    curShot ++;
+                    if(curShot == ShotPattern.childCount)   {
+                        curShot = 0;
+                    }
+                }
+                else
+                {
+                    Transform ShotPattern = transform.GetChild(0);
+                    for (int i = 0; i < ShotPattern.childCount; i++)    {
+                        Transform shotPosition = ShotPattern.GetChild(i);
+                        monster.Shot (shotPosition);
+                    }
+                }
+
+                timerShot = 0;
+            }
+            else{
+                timerShot += 1;
+            }
+        }
+    }
+
+}
+
