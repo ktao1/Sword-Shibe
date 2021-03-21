b4e06efcec4fad4160fe467decc263a0f43ab3c1
ktao1 <60584045+ktao1@users.noreply.github.com>
2021-03-21 16:44:53 -0400

    Finished Shirime AI

---
+++ Assets/Scripts/Enemy/EnemyAI/Shirime/ShirimeBulletGFXTurning.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShirimeBulletGFXTurning : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if monster move to the left
+        if (rb.velocity.x < 0f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (rb.velocity.x > 0f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+    }
+}
