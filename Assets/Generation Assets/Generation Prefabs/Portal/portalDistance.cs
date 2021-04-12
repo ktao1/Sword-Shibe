2bf17bb19f20a30df817341bb8e05da8ccf4d42b
maggiePoletti <mpolett1@umbc.edu>
2021-04-12 14:17:27 -0400

    implemented portal animations

---
+++ Assets/Generation Assets/Generation Prefabs/Portal/portalDistance.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portalDistance : MonoBehaviour
+{
+GameObject player;
+public Animator animator;
+public double viewDistance = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(player.transform.position, transform.position)<viewDistance)
+        {
+            animator.SetBool("playerNear", true);
+        }
+        else
+        {
+            animator.SetBool("playerNear", false);
+        }
+    }
+}
