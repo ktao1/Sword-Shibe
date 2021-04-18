5d5897295ae60371dca3c588cc61ce1ee6c66bc6
meowkun <ktao1@umbc.edu>
2021-04-18 16:54:02 -0400

    Update Player.cs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -484,7 +484,7 @@ public class Player : MonoBehaviour
         ChangeAnimationState(idleAnimation);
         sr.color = new Color(255, 255, 255);
         isTakeingDamage = false;
-        isInvincible = true;
+        // isInvincible = true;
     }
 
 
@@ -513,7 +513,7 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Bullet" && isDashing)
+        if((collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Bullet") && isDashing)
         {
             this.GetComponent<BoxCollider2D>().enabled = false;
         }
