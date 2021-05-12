8213fecc6cb27a650abd185ec1913229517377a9
meowkun <ktao1@umbc.edu>
2021-05-12 14:55:56 -0400

    load screen

--- Assets/Scripts/Level/SceneLoading/SceneLoading.cs
+++ Assets/Scripts/Level/SceneLoading/SceneLoading.cs
@@ -2,30 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 using UnityEngine.UI;
 
 public class SceneLoading : MonoBehaviour
 {
+    /*
     [SerializeField]
     private Image progressBar;
-
+    */
+    [SerializeField]
+    public VideoPlayer videoPLayer;
     // Start is called before the first frame update
     void Start()
     {
         //Start Async Scene Load
-        StartCoroutine(LoadAsyncOperation());
+        //StartCoroutine(LoadAsyncOperation());
+        videoPLayer.loopPointReached += EndReached;
+
     }
-    
+    void EndReached(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
     IEnumerator LoadAsyncOperation()
     {
+        /*
         //Begin loading next scene
         AsyncOperation gameLevel = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
 
         while(gameLevel.progress < 1)
         {
             progressBar.fillAmount = gameLevel.progress;
+            wait();
             yield return new WaitForEndOfFrame();
+
         }
         
+        */
+        wait();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        yield return new WaitForEndOfFrame();
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(5);
     }
 }
