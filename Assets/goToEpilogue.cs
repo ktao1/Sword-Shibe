3189f5d32f1b9c9029ddcbee1eccbb5700629a15
Samuel Tayman <sata1@umbc.edu>
2021-05-13 18:04:12 -0400

    Added transition to epilogue

---
+++ Assets/goToEpilogue.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goToEpilogue : MonoBehaviour
+{
+    private GameObject finalBoss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        finalBoss = GameObject.Find("Komainu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(finalBoss == null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
+    }
+}
