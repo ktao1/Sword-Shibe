5862fdd448f7b56f2f418e9b4176246dce1c3d12
Samuel Tayman <sata1@umbc.edu>
2021-05-17 19:50:09 -0400

    Skip button added
    -Skip button added and script to change control layout added.

---
+++ Assets/SwitchControls.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchControls : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite controllerLayout;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        if(Input.GetJoystickNames().Length > 0)
+        {
+            GetComponent<Image>().sprite = controllerLayout;
+        }
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
