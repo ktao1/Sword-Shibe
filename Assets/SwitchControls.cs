9ff9d4f9e535b3907e7e3a16688cfe92908b5a04
Samuel Tayman <nicholastayman@live.com>
2021-05-19 15:41:01 -0400

    Controller swap on unplug

--- Assets/SwitchControls.cs
+++ Assets/SwitchControls.cs
@@ -8,6 +8,9 @@ public class SwitchControls : MonoBehaviour
     [SerializeField]
     private Sprite controllerLayout;
 
+    [SerializeField]
+    private Sprite keyboardLayout;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +25,17 @@ public class SwitchControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetJoystickNames().Length > 0 && Input.GetJoystickNames()[0].Length > 0)
+        {
+            GetComponent<Image>().sprite = controllerLayout;
+            if (GameObject.Find("Menus") != null)
+                GameObject.Find("Menus").SetActive(false);
+        }
+        else
+        {
+            GetComponent<Image>().sprite = keyboardLayout;
+            if (GameObject.Find("Menus") != null)
+                GameObject.Find("Menus").SetActive(true);
+        }
     }
 }
